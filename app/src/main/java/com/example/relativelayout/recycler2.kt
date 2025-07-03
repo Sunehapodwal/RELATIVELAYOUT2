b9701248e400f98dcd471ab72230d0278f7d9034
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-03 11:42:20 +0530

    Create recycler2.kt

---
+++ app/src/main/java/com/example/relativelayout/recycler2.kt
@@ -0,0 +1,9 @@
+package com.example.relativelayout
+
+import android.content.ClipDescription
+
+data class recycler2(
+    var id:Int = 0,
+var title:String?=null,
+    var description:String?=null,
+)
