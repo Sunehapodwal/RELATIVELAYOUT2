28bb54e5bbd4d7f73463e5bee40cf0eda7f763ed
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-07 17:59:09 +0530

    Create navigateactivity.kt

---
+++ app/src/main/java/com/example/relativelayout/navigateactivity.kt
@@ -0,0 +1,23 @@
+package com.example.relativelayout
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class navigateactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_navigateactivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val title= intent.getStringExtra("key")
+        Toast.makeText(this,title, Toast.LENGTH_SHORT).show()
+    }
+}
