892e83a7cfb0311ddf38f3e5afcafaf103ab1476
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-07 17:59:15 +0530

    Update RecyclerAdaptor.kt

--- app/src/main/java/com/example/relativelayout/RecyclerAdaptor.kt
+++ app/src/main/java/com/example/relativelayout/RecyclerAdaptor.kt
@@ -7,41 +7,45 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdaptor(var list :ArrayList<recycler2>)
-    : RecyclerView.Adapter<RecyclerAdaptor.Viewholder>() {
-    class Viewholder(var view: View) : RecyclerView.ViewHolder(view) {
-        var name = view.findViewById<TextView>(R.id.text1)
-        var update = view.findViewById<Button>(R.id.btnupdate)
-        var delete = view.findViewById<Button>(R.id.btndelete)
+class RecyclerAdapter(var list :ArrayList<recycler2>,var clickInterface: OnClick
+)
+    :RecyclerView.Adapter<RecyclerAdapter.Viewholder>(){
+    class Viewholder (var view: View):RecyclerView.ViewHolder(view){
+        var name=view.findViewById<TextView>(R.id.text1)
+        var update=view.findViewById<Button>(R.id.btnupdate)
+        var delete=view.findViewById<Button>(R.id.btndelete)
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): Viewholder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.itemlayout, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.Viewholder {
+        var view=LayoutInflater.from(parent.context).inflate(R.layout.itemlayout,parent,false)
         return Viewholder(view)
 
     }
 
-    override fun onBindViewHolder(holder: RecyclerAdaptor.Viewholder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerAdapter.Viewholder, position: Int) {
         holder.apply {
             name.setText(list[position].title)
             update.setOnClickListener {
+                clickInterface.update(position)
             }
             delete.setOnClickListener {
+                clickInterface.delete(position)
+            }
+            view.setOnClickListener {
+                clickInterface.onItemClick(position)
             }
         }
     }
 
-
     override fun getItemCount(): Int {
-        return list.size
+        return  list.size
     }
+    interface OnClick{
 
-    interface OnClick {
         fun update(position: Int)
         fun delete(position: Int)
+        //Click on particular item
+        fun onItemClick(position: Int)
     }
-
 }
+
